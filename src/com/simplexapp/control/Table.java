8f5c420cf42219e5c500959d8cae1d0de367b799
jorgemauro <jorgemauro1000@hotmail.com>
2017-10-08 08:08:47 -0300

    tirando so prints

--- src/com/simplexapp/control/Table.java
+++ src/com/simplexapp/control/Table.java
@@ -69,25 +69,13 @@ public class Table {
         Cell cell;
         for(int i=0;i<matrix.length;i++){
             cell=matrix[i][colAccept];
-            System.out.println("CellCol: ");
-            Change.PrintCell(cell);
-            System.out.println("");
-            System.out.println("Marcado: "+cell.getCellInf());
-            System.out.println("line: "+i);
-            System.out.println("--------------------------------------------------");
             cell.setChkCellInf(true);
         }
     }
     public void chkCellLineSup(int lineAccept){
         Cell cell;
-        for(int i=0;i<matrix[i].length;i++){
+        for(int i=0;i<matrix[1].length;i++){
             cell=matrix[lineAccept][i];
-
-            System.out.println("CellLine: ");
-            Change.PrintCell(cell);
-            System.out.println("");
-            System.out.println("Marcado: "+cell.getCellSup());
-            System.out.println("col: "+i);
             cell.setChkCellSup(true);
         }
     }
